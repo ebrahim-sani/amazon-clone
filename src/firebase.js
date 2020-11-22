442cafd043730ac5665cbcccbc0569aa441896d7
ebrahim-sani <ibraheemsani035@gmail.com>
2020-11-22 02:15:54 +0100

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAoLRPZfHnyXQ1ImGyCUmVa2SPxc_GCKNw",
+  authDomain: "clone-11c85.firebaseapp.com",
+  databaseURL: "https://clone-11c85.firebaseio.com",
+  projectId: "clone-11c85",
+  storageBucket: "clone-11c85.appspot.com",
+  messagingSenderId: "1005534138843",
+  appId: "1:1005534138843:web:81f93bec8cc822ede0d7fe",
+};
