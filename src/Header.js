587e20fa39f32ad250dee1cf90f754d3188e3b00
ebrahim-sani <ibraheemsani035@gmail.com>
2020-12-24 08:46:10 +0100

    Built headerTwo

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
@@ -15,48 +19,107 @@ function Header() {
   };
   return (
     <div className="header">
-      <Link to="/">
-        <img
-          className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          alt=""
-        />
-      </Link>
+      <div className="header__one">
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            alt=""
+          />
+        </Link>
 
-      <div className="header__search">
-        <input className="header__searchInput" type="text" />
-        <SearchIcon className="header__searchIcon" />
-      </div>
-      <div className="header__nav">
-        <Link to={!user && "/login"}>
-          <div onClick={handleAuthentication} className="header__option">
-            <span className="optionLineOne">
-              Hello {!user ? "Guest" : user.email}
-            </span>
-            <span className="optionLineTwo">
-              {user ? "Sign Out" : "Sign In"}
-            </span>
+        <div className="header__add">
+          <div className="addLocation__icon">
+            <LocationOnIcon className="icon" />
           </div>
-        </Link>
-        <Link to="/orders">
-          <div className="header__option">
-            <span className="optionLineOne">Returns</span>
-            <span className="optionLineTwo">& Orders</span>
+          <div className="addText">
+            <span className="addText__one">Deliverd to Ebrahim</span>
+            <span className="addText__two">Biu 603108</span>
           </div>
-        </Link>
-        <div className="header__option">
-          <span className="optionLineOne">Your</span>
-          <span className="optionLineTwo">Prime</span>
         </div>
 
-        <Link to="/checkout">
-          <div className="header__optionBasket">
-            <ShoppingCartIcon />
-            <span className="optionLineTwo header__basketCount">
-              {basket?.length}
+        <div className="header__search">
+          <div className="searchField__left">
+            <span className="searchText">
+              All <KeyboardArrowDownIcon className="arrow" />
             </span>
           </div>
-        </Link>
+          <input className="header__searchInput" type="text" />
+          <SearchIcon className="header__searchIcon" />
+        </div>
+
+        <div className="headerFlag">
+          <img className="flag" src="usa.png" alt="" />
+          <KeyboardArrowDownIcon className="flagArrow" />
+        </div>
+
+        <div className="header__nav">
+          <Link to={!user && "/login"}>
+            <div onClick={handleAuthentication} className="header__option">
+              <span className="optionLineOne">
+                Hello {!user ? "Guest" : user.email}
+              </span>
+              <span className="optionLineTwo">
+                {user ? "Sign Out" : "Sign In"}
+              </span>
+            </div>
+          </Link>
+          <Link to="/orders">
+            <div className="header__option">
+              <span className="optionLineOne">Returns</span>
+              <span className="optionLineTwo">& Orders</span>
+            </div>
+          </Link>
+          <div className="header__option">
+            <span className="optionLineOne">Your</span>
+            <span className="optionLineTwo">Prime</span>
+          </div>
+
+          <Link to="/checkout">
+            <div className="header__optionBasket">
+              <ShoppingCartIcon />
+              <span className="optionLineTwo header__basketCount">
+                {basket?.length}
+              </span>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      <div className="header__two">
+        <div className="nav__left">
+          <MenuIcon />
+          <span className="icon__lebel">All</span>
+        </div>
+        <div></div>
+        <div className="headerTwo__container">
+          <div className="nav__link">
+            <span className="header_text">Today's Deals</span>
+          </div>
+
+          <div className="nav__link">
+            <span className="header_text">users Amazon.com</span>
+          </div>
+
+          <div className="nav__link">
+            <span className="header_text">Customer Services</span>
+          </div>
+
+          <div className="nav__link">
+            <span className="header_text">Browsing History</span>
+          </div>
+
+          <div className="nav__link">
+            <span className="header_text">Buy Again</span>
+          </div>
+
+          <div className="nav__link">
+            <span className="header_text">Gift Cards</span>
+          </div>
+        </div>
+        <div className="nav_right">
+          <span className="navLeft_a">Amazon's response to COVID-19</span>
+        </div>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/HolidayDeals/Promo/Desktop/Fuji_TallHero_HD_NG_en_US_2x._CB414678880_.jpg"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/HolidayDeals/Promo/Desktop/Fuji_TallHero_HD_NG_en_US_1x._CB414678881_.jpg"
           alt=""
         />
         <div className="home__row">
